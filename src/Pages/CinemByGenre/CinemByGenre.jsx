fefc0e616a2d737449cd74d86244bda249029224
Sacre-MBIKU <sajombiku@gmail.com>
2021-06-13 16:57:55 +0100

    style: set cards container wrapper

--- src/Pages/CinemByGenre/CinemByGenre.jsx
+++ src/Pages/CinemByGenre/CinemByGenre.jsx
@@ -1,10 +1,19 @@
+import CardsContainer from "../../Components/CardsContainer/CardsContainer";
 import Navbar from "../../Layouts/Navbar/Navbar";
 import CinemByGenreDecoration from "./CinemByGenreDecoration";
 
 const CinemByGenre = ({ genre }) => {
-  return <CinemByGenreDecoration>
-      <Navbar secondAppMode = {true}/>
-  </CinemByGenreDecoration>;
+  return (
+    <CinemByGenreDecoration>
+      <Navbar secondAppMode={true} />
+      <main>
+        <div className="cards-container-wrapper">
+          <CardsContainer />
+          <div className="filter"></div>
+        </div>
+      </main>
+    </CinemByGenreDecoration>
+  );
 };
 
 export default CinemByGenre;
--- src/Pages/CinemByGenre/CinemByGenreDecoration.jsx
+++ src/Pages/CinemByGenre/CinemByGenreDecoration.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {  SECOND_COLOR } from "../../Shared_Components/GlobalStyles.jsx/GlobalStyles";
 
 const CinemByGenreDecoration = styled.div`
   width: 100%;
@@ -8,5 +9,20 @@ const CinemByGenreDecoration = styled.div`
   align-items: center;
   justify-content: space-between;
   z-index: 100;
+
+  main {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 0 5%;
+
+    .filter {
+      width: 30%;
+      height: 10rem;
+      background-color: #0a0c1d;
+      border: 3px solid ${SECOND_COLOR};
+    }
+  }
 `;
 export default CinemByGenreDecoration;
